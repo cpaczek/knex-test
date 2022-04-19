e0d638e349ea44c1741987d41196c23f9ed86402
Cameron Paczek <cpazek@gmail.com>
2022-04-19 01:31:38 -0700

    minimize test case

--- index.js
+++ index.js
@@ -9,27 +9,15 @@ const knex = require('knex')({
     },
     debug: true
 });
-const {isNil, prop, omit, castArray} = require('lodash/fp');
 (async () => {
 try {
-    let object = {
-        name: 'id',
-        type: 'increments',
-        args: [ { primary: true, primaryKey: true } ],
-        defaultTo: null,
-        notNullable: true,
-        unsigned: false
-    }
-    // Create a table
     await knex.schema
         .createTable('knex_test', table => {
             table.increments('id').notNullable();
             table.string('user_name');
         })
-        // ...and another
-
     await knex.schema.alterTable('knex_test', table => {
-        createColumn(table, object).alter({ alterNullable: false, alterType: true });
+        table.increments('id').alter({ alterNullable: false, alterType: true });
     })
 
     // Finally, add a catch statement
@@ -37,30 +25,3 @@ try {
     console.error(e);
 }
 })();
-
-const createColumn = (tableBuilder, column) => {
-    const { type, name, args = [], defaultTo, unsigned, notNullable } = column;
-
-    const col = tableBuilder[type](name, ...args);
-
-    if (unsigned === true) {
-        col.unsigned();
-    }
-    //
-    if (!isNil(defaultTo)) {
-        const [value, opts] = castArray(defaultTo);
-
-        if (prop('isRaw', opts)) {
-            col.defaultTo(db.connection.raw(value), omit('isRaw', opts));
-        } else {
-            col.defaultTo(value, opts);
-        }
-    }
-    if (notNullable === true) {
-        col.notNullable();
-    } else {
-        col.nullable();
-    }
-
-    return col;
-};
